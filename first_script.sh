264ff5507eb0c412ad4d82da6d0df072192f72b5
abdykadyrrova <zhyldyzabdykadyrova@gmail.com>
2023-03-15 00:54:42 +0000

    comments

--- first_script.sh
+++ first_script.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "Hello World"
+echo "this script name is $0" ## $0 represents script itself
+
+echo "Hi $1 $2" ## totsl of 9 arguments can be used 
+
+echo "list of all arguments: $@" ## will write every argument 
+
+echo "PID: $$" ## will show process ID of the command
